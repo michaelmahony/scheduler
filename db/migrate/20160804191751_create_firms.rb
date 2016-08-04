eb0d1a7939ae66931daf06f693117631ba4f7a15
michaelmahony <michael.mahony@umbc.edu>
2016-08-04 17:57:19 -0400

    Began to define the Firms model.

--- db/migrate/20160804191751_create_firms.rb
+++ db/migrate/20160804191751_create_firms.rb
@@ -1,6 +1,24 @@
 class CreateFirms < ActiveRecord::Migration[5.0]
   def change
     create_table :firms do |t|
+      t.String :name
+      t.String :phone_number
+
+      t.String :mon_open_time
+      t.String :mon_close_time
+      t.String :tue_open_time
+      t.String :tue_close_time
+      t.String :wed_open_time
+      t.String :wed_close_time
+      t.String :thu_open_time
+      t.String :thu_close_time
+      t.String :fri_open_time
+      t.String :fri_close_time
+      t.String :sat_open_time
+      t.String :sat_close_time
+      t.String :sun_open_time
+      t.String :sun_close_time
+
 
       t.timestamps
     end
